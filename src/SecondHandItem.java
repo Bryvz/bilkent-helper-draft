5e17de2f5524402b7c36c14543d726a569059e2d
efe eroğlu <eroglu.efe@ug.bilkent.edu.tr>
2025-05-08 20:39:59 +0300

    User and SeconHandItem class added. (still wip)

---
+++ src/SecondHandItem.java
@@ -0,0 +1,22 @@
+public class SecondHandItem {
+    private String itemId;
+    private String name;
+    private double price;
+    private String course;
+    private String sellerId;
+    private String photo;
+    private boolean isSold;
+    
+    public void delete(){
+
+    }
+
+    public void uploaddPhoto(String image){
+
+    }
+
+    public void markAsSoldd(){
+
+    }
+
+}
