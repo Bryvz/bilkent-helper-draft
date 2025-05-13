aea2a5a38d115c108d43a67c689e18ba41e9c453
efe eroğlu <eroglu.efe@ug.bilkent.edu.tr>
2025-05-13 21:03:34 +0300

    editProfile some functionality

--- src/ProfilePage.java
+++ src/ProfilePage.java
@@ -1,12 +1,27 @@
+import javax.smartcardio.Card;
 import javax.swing.*;
 import javax.swing.border.Border;
 
 import java.awt.*;
 import java.awt.event.ComponentEvent;
+import java.io.File;
 import java.security.spec.EdDSAParameterSpec;
+import java.util.ArrayList;
+
 
 public class ProfilePage extends BackgroundPanel2 {
+    private ArrayList<JButton> buttons = new ArrayList<>();
+    JPanel invisiblePanel = new JPanel();
+
+    
+    
+   
+    
     public ProfilePage(AppFrame frame) {
+        
+        
+
+        
         setLayout(new BorderLayout());
 
         int marginX = (int) (Toolkit.getDefaultToolkit().getScreenSize().width * 0.04);
@@ -64,7 +79,7 @@ public class ProfilePage extends BackgroundPanel2 {
         profilePanel.setLayout(new BoxLayout(profilePanel, BoxLayout.Y_AXIS));
 
         ImageIcon profilePic = new ImageIcon("logos/placeholder_profile_photo.png");
-        profilePic = resizeIcon(profilePic, 200, 200);
+        profilePic = resizeIcon(profilePic, 150, 150);
         
         JLabel picLabel = new JLabel(profilePic);
         JLabel usernameLabel = new JLabel("User1234");
@@ -90,7 +105,9 @@ public class ProfilePage extends BackgroundPanel2 {
             button.setBorderPainted(false);
             button.setForeground(Color.WHITE);
             button.setFont(new Font("SansSerif", Font.PLAIN, 18));
+            buttons.add(button);
 
+            JPanel editProfilePanel = createEditProfilePanel();
             button.addMouseListener(new java.awt.event.MouseAdapter() {
                 @Override
                 public void mouseEntered(java.awt.event.MouseEvent evt) {
@@ -104,16 +121,20 @@ public class ProfilePage extends BackgroundPanel2 {
                  @Override
                 public void mouseClicked(java.awt.event.MouseEvent evt) {
                     if (button.getText().equals("<html><u>Favorites</u></html>")){
+                         editProfilePanel.setVisible(false);
                         createFavoritePanel();
                     }
                     else if ( button.getText().equals("<html><u>Sell Books</u></html>")){
+                         editProfilePanel.setVisible(false);
                         // sellBooksPanel();
                     }
                     else if ( button.getText().equals("<html><u>Show My Ads</u></html>")){
+                         editProfilePanel.setVisible(false);
                         // showMyAdsPanel();
                     }
                     else if(button.getText().equals("<html><u>Edit Profile</u></html>")){
-                        // editProfilePanel();
+                        
+                         editProfilePanel.setVisible(true);
                     }
                 }
             });
@@ -124,6 +145,8 @@ public class ProfilePage extends BackgroundPanel2 {
         }
 
         
+       
+
 
         this.add(sidebar, BorderLayout.WEST);
 
@@ -159,9 +182,12 @@ public class ProfilePage extends BackgroundPanel2 {
                     
                 }
             });
+
+            
+            
     }
     
-
+    
     public static ImageIcon resizeIcon(ImageIcon icon, int width, int height){
         Image image = icon.getImage();
         Image resizedImage = image.getScaledInstance(width, height, Image.SCALE_SMOOTH);
@@ -170,6 +196,132 @@ public class ProfilePage extends BackgroundPanel2 {
     }
 
     public void createFavoritePanel(){
+        
         System.out.println("Im clicked");
+
     }
+    
+    public JPanel createEditProfilePanel(){
+        
+        class RoundedTextField extends JTextField {
+        public RoundedTextField(int width, int height) {
+            setPreferredSize(new Dimension(width, height));
+            setOpaque(false);
+            setBorder(BorderFactory.createEmptyBorder(10, 15, 10, 15));
+        }
+
+        @Override
+        protected void paintComponent(Graphics g) {
+            Graphics2D g2 = (Graphics2D) g.create();
+            g2.setColor(Color.BLACK);
+            g2.fillRoundRect(0, 0, getWidth(), getHeight(), getHeight(), getHeight());
+            super.paintComponent(g);
+            g2.dispose();
+        }
+
+        @Override
+        protected void paintBorder(Graphics g) {
+            Graphics2D g2 = (Graphics2D) g.create();
+            g2.setColor(Color.WHITE);
+            g2.setStroke(new BasicStroke(2));
+            g2.drawRoundRect(0, 0, getWidth() - 1, getHeight() - 1, getHeight(), getHeight());
+            g2.dispose();
+        }
+    }
+
+
+        class RoundedPasswordField extends JPasswordField {
+        public RoundedPasswordField(int width, int height) {
+            setPreferredSize(new Dimension(width, height));
+            setOpaque(false);
+            setBorder(BorderFactory.createEmptyBorder(10, 15, 10, 15));
+        }
+
+        @Override
+        protected void paintComponent(Graphics g) {
+            Graphics2D g2 = (Graphics2D) g.create();
+            g2.setColor(Color.BLACK);
+            g2.fillRoundRect(0, 0, getWidth(), getHeight(), getHeight(), getHeight());
+            super.paintComponent(g);
+            g2.dispose();
+        }
+
+        @Override
+        protected void paintBorder(Graphics g) {
+            Graphics2D g2 = (Graphics2D) g.create();
+            g2.setColor(Color.WHITE);
+            g2.setStroke(new BasicStroke(2));
+            g2.drawRoundRect(0, 0, getWidth() - 1, getHeight() - 1, getHeight(), getHeight());
+            g2.dispose();
+        }
+    }
+        
+        JPanel mainBox = new JPanel() {
+            @Override
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                Graphics2D g2 = (Graphics2D) g.create();
+                g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+                g2.setColor(Color.BLACK);
+                g2.fillRoundRect(0, 0, getWidth(), getHeight(), 60, 60);
+                g2.dispose();
+            }
+        };
+        mainBox.setOpaque(false);
+        mainBox.setPreferredSize(new Dimension(200, 400));
+        mainBox.setLayout(new GridLayout(3, 1));
+        mainBox.setVisible(false);
+
+        JButton editPhoto = new JButton("Edit profile photo: ");
+        editPhoto.setSize(10,10);
+        editPhoto.addActionListener(e -> choosePhoto());
+
+        mainBox.add(editPhoto);
+
+        // Name TextField
+        RoundedTextField nameField = new RoundedTextField(250, 55);
+        nameField.setFont(new Font("Avenir Next", Font.PLAIN, 14));
+        nameField.setForeground(Color.WHITE);
+        nameField.setCaretColor(Color.WHITE);
+        nameField.setText("Name:");
+
+        JPanel passwordAndOk = new JPanel();
+        // Password TextField (Şifre)
+        RoundedPasswordField passwordField = new RoundedPasswordField(250, 55);
+        passwordField.setFont(new Font("Avenir Next", Font.PLAIN, 14));
+        passwordField.setForeground(Color.WHITE);
+        passwordField.setCaretColor(Color.WHITE);
+        passwordField.setEchoChar((char) 0); 
+        passwordField.setText(""); 
+
+        
+        passwordField.setText("Password:");  
+        passwordAndOk.add(passwordField);
+        JButton okButton = new JButton("Save Changes");
+        // change the users name and password to be impemented.
+        passwordAndOk.add(okButton);
+        
+        mainBox.add(nameField);
+        mainBox.add(passwordField);
+
+        this.add(mainBox, BorderLayout.CENTER);
+        
+        return mainBox;
+        
+    }
+
+    public void choosePhoto(){
+        JFileChooser fileChooser = new JFileChooser();
+        fileChooser.setDialogTitle("Select profile photo:");
+
+        int result = fileChooser.showOpenDialog(this);
+        if ( result == JFileChooser.APPROVE_OPTION){
+            File selectedFile = fileChooser.getSelectedFile();
+            
+            ImageIcon icon = new ImageIcon(selectedFile.getAbsolutePath());
+            Image scaled = icon.getImage().getScaledInstance(200, 200, Image.SCALE_SMOOTH);
+            
+        }
+    }
+    
 }
